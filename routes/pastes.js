b216e411a2d071bd730c17eeee1f9b0eed3e8954
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2026-01-28 14:41:11 +0530

    uuid package error check

--- routes/pastes.js
+++ routes/pastes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
 // const Paste = require('../models/Paste');
 // const { getNow } = require('../utils/time');
