7a51f63a1f7915426031ddf41ec55c2b5149a631
maqsoodhussain <maqsoodhussain@gdcbeerwah.edu.in>
2026-01-28 14:48:04 +0530

    uuid package error check- replace with crypto default

--- routes/pastes.js
+++ routes/pastes.js
@@ -6,9 +6,9 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get("/test",(req,res)=>{
-  return res.status(200).json({ ok: true, message: 'wroking' });
-})
+app.get('/test', (req, res) => {
+  res.json({ id: crypto.randomUUID() });
+});
 
 
 
